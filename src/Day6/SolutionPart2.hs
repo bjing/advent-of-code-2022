771680f0c65bf112ef45a63c54395c3b105c29c4
Brian Jing <brian.jing.dev@gmail.com>
2025-08-27 16:20:03 +1000

    Add solutioin to day 6 part2

--- src/Day6/SolutionPart2.hs
+++ src/Day6/SolutionPart2.hs
@@ -1,9 +1,9 @@
-module Day6.SolutionPart1 where
+module Day6.SolutionPart2 where
 
 import qualified Data.Set as Set
 
-runDay6Part1 :: IO ()
-runDay6Part1 = do
+runDay6Part2 :: IO ()
+runDay6Part2 = do
     content <- loadInput "src/Day6/input.txt"
     let result = process content
     print result
@@ -17,9 +17,9 @@ process = process' 0
 
 process' :: Int -> String -> Int
 process' charsProcessed s = 
-    if allUnique $ take 4 s
+    if allUnique $ take 14 s
     then
-        charsProcessed + 4
+        charsProcessed + 14
     else
         process' (charsProcessed + 1) (tail s)
 
